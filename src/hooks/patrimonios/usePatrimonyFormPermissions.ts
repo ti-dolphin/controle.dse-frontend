ce7c9968f13e03d19b9f631afde9221854b365ec
Leonardo Pias da  Rocha <leorocha2004@gmail.com>
2026-04-28 13:25:08 -0300

    ajuste para perm_estoque

--- src/hooks/patrimonios/usePatrimonyFormPermissions.ts
+++ src/hooks/patrimonios/usePatrimonyFormPermissions.ts
@@ -8,21 +8,21 @@ export const usePatrimonyFormPermissions = (mode : string, patrimony : Partial<P
     const [permissionToEdit, setPermissionToEdit] = useState(false);
 
     useEffect(( ) => { 
-         if (mode === "create") {
-           setPermissionToEdit(true);
-           return;
-         }
-         if (user && patrimony) {
-           setPermissionToEdit(false);
-           const responsableForType =
-             Number(user?.CODPESSOA) ===
-             Number(patrimony?.tipo_patrimonio?.responsavel_tipo);
-           const adm = Number(user?.PERM_ADMINISTRADOR) === 1;
-           if ((responsableForType && mode === "edit") || adm) {
-             setPermissionToEdit(true);
-             return;
-           }
-         }
+      if (mode === "create") {
+        setPermissionToEdit(true);
+        return;
+      }
+      if (user && patrimony) {
+        setPermissionToEdit(false);
+        const responsableForType =
+          Number(user?.CODPESSOA) ===
+          Number(patrimony?.tipo_patrimonio?.responsavel_tipo);
+        const canEdit = Number(user?.PERM_ADMINISTRADOR) === 1 || Number(user?.PERM_ESTOQUE) === 1
+        if ((responsableForType && mode === "edit") || canEdit) {
+          setPermissionToEdit(true);
+          return;
+        }
+      }
     }, [user, patrimony, mode]);
 
     return { permissionToEdit };
