0b4ac3d50b227e18453b15cd681cbfe94ba6ed58
Leonardo Pias da  Rocha <leorocha2004@gmail.com>
2026-05-14 09:08:23 -0300

    atualizar baseURL para o endereço correto

--- src/apiLocal.ts
+++ src/apiLocal.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiLocal = axios.create({
-  baseURL: "http://192.168.68.129:8080/api",
+  baseURL: "http://192.168.68.124:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
